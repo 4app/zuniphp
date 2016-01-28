f102f862985f80621c836a43747af0689483f379
Estefanio NS <estefanions@gmail.com>
2016-01-28 21:21:47 -0200

    change view Welcome

--- app/View/index.php
+++ app/View/index.php
@@ -2,97 +2,48 @@
 <html lang="en">
     <head>
       <meta charset="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Welcome to ZuniPHP</title>
-
-        <style type="text/css" media="all">
-
-            *{ margin:0; padding: 0 }
-
-            body, code{
-                    color: #444;
-                    font-family: arial;
-                    line-height: 22px
-                }
-
-            h1{font-size: 25px; margin: 0 0 15px; color: #09a}
-
-            a{color: #05c; text-decoration: none}
-            a:hover{color: #07c}
-
-            p{margin: 0 0 15px}
-
-            code{color: #000 }
-            .code{
-                color: #060;
-                background: #f8f8f8;
-                border: 1px solid #ccc;
-                margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-            }
-
-            .content {
-                width: 80%;
-                float: left;
-                margin: 10px 10%;
-                border: 1px solid #ccc;
-
-                padding: 10px;
-                
-                -webkit-border-radius: 5px;
-                -moz-border-radius: 5px;
-                border-radius: 5px;
-
-                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-                
-            }
-
-            .header_page{
-                width: 100%;
-                float: left;
-                margin: 0 0 20px;
-                padding: 0 0 20px;
-                border-bottom: 1px solid #ccc
-            }
-
-
-
-            
-        </style>
-
-
+<style type="text/css" media="all">
+    *{margin:0; padding: 0}
+    body{
+        color: #555; background: #f8f8f8;
+        font-family:Verdana, Arial, Helvetica, sans-serif 
+    }
+    h1,p{ margin: 0 0 15px }
+    h1 {color: #09a; font-size: 35px }
+    code{color: #040}
+    a{color: #09c}
+    .box{ width: 500px; margin: 0 auto }
+    @media (max-width: 600px) { 
+        h1 {font-size: 25px}
+        .box  { width: 80%; padding: 0 10px } 
+    }
+   
+</style>
     </head>
 <body>
-
-<div class="content">
-
-    <div class="header_page">
-        <h1>Welcome to ZuniPHP!</h1>
-    </div>
-
-   
+<div class="box">
+        <h1>Welcome to ZuniPHP</h1>
     <p>
-        the example of view are in:
+        The page is being generated dynamically by ZuniPHP.<br />
+        you'll find it located at: <br />
+        <code> app/View/index.php </code>
     </p>
-
-    <p class="code">
-        <code>
-            app/View/index.php
-        </code>
-    </p>
-
     <p>
-        the example of view are in:
+        The corresponding controller for this page is found at: <br />
+        <code> app/Controller/Index.php </code>
     </p>
-    <p class="code">
-        <code>
-            app/Controller/Index.php
-        </code>
+    <p>
+        <a target="_blank" href="http://4app.github.io/zuniphp/">ZuniPHP</a>
     </p>
-        
-<p>
-    page: <a target="_blank" href="http://4app.github.io/zuniphp/">http://4app.github.io/zuniphp/</a>
-</p>
-</div>    
+
+
+</div>
+
+<!--[if lt IE 9]>
+<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 
 </body>
 </html> 
