87e3bbd6a4f9be76850a6048f7097c874210cc53
Estefanio NS <estefanions@gmail.com>
2016-03-05 00:53:51 -0300

    change html

--- app/View/index.php
+++ app/View/index.php
@@ -4,46 +4,9 @@
       <meta charset="UTF-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Welcome to ZuniPHP</title>
-<style type="text/css" media="all">
-    *{margin:0; padding: 0}
-    body{
-        color: #555; background: #fff;
-        font-family:Verdana, Arial, Helvetica, sans-serif 
-    }
-    h1,p{ margin: 0 0 15px }
-    h1 {color: #09c; font-size: 35px }
-    code{color: #040}
-    a{color: #09c}
-    .box{ width: 500px; margin: 0 auto }
-    @media (max-width: 600px) { 
-        h1 {font-size: 25px}
-        .box  { width: 80%; padding: 0 10px } 
-    }
-   
-</style>
     </head>
 <body>
-<div class="box">
-        <h1>Welcome to ZuniPHP</h1>
-    <p>
-        The page is being generated dynamically by ZuniPHP.<br />
-        you'll find it located at: <br />
-        <code> app/View/index.php </code>
-    </p>
-    <p>
-        The corresponding controller for this page is found at: <br />
-        <code> app/Controller/Index.php </code>
-    </p>
-    <p>
-        <a target="_blank" href="http://4app.github.io/zuniphp/">ZuniPHP</a>
-    </p>
-
-
-</div>
-
-<!--[if lt IE 9]>
-<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-
+<h1>Welcome to ZuniPHP</h1>
+page example
 </body>
 </html> 
